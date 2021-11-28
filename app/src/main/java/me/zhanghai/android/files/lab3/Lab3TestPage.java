cbfb11782cac69a61caf868a47f32f2898830b45
Roc136 <1361050885@qq.com>
2021-11-28 19:56:43 +0800

    添加返回按钮，对新界面按钮增加功能

--- app/src/main/java/me/zhanghai/android/files/lab3/Lab3TestPage.java
+++ app/src/main/java/me/zhanghai/android/files/lab3/Lab3TestPage.java
@@ -2,7 +2,11 @@ package me.zhanghai.android.files.lab3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import java.util.Random;
 
 import me.zhanghai.android.files.R;
 import me.zhanghai.android.files.app.AppActivity;
@@ -13,5 +17,19 @@ public class Lab3TestPage extends AppActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab3_test_page);
+    }
+
+    public void clickTestButton(View view) {
+        Log.d("lab3", "Click Test Button:" + String.valueOf(view.getId()));
+        Random random = new Random();
+        int r = random.nextInt(255);
+        int g = random.nextInt(255);
+        int b = random.nextInt(255);
+        view.setBackgroundColor(Color.rgb(r, g, b));
+    }
+
+    public void clickBackButton(View view) {
+        Log.d("lab3", "Click Back Button");
+        finish();
     }
 }
